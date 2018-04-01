3fbe5d4e2e8bf85b2c967736460dca7941696f8c
Zhitao Gong <zhitaao.gong@gmail.com>
2018-04-01 17:40:04 -0500

    refactor code to enable editable install locally

---
+++ README.md
@@ -0,0 +1,23 @@
+Tensorflow Models
+=================
+
+> What I cannot create, I do not understand.  -- Feynman
+
+This repo contains TensorFlow re-implementation of interesting models from
+random papers I read.  I use the low-level API as much as possible since
+research models almost never fit into the standard API.
+
+## Getting Started
+
+Personally I use [*editable installs*](https://pip.pypa.io/en/stable/reference/pip_install/#editable-installs) (i.e., `python install -e
+path/to/tfmodels`) to install the modules locally in a virtual environment
+created with [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/latest/).  It should be easy to integrate code from
+this repo to your project simply by copy-and-paste.
+
+## Model List
+
+- Text classification.  Word-level CNN models [@kim2014-convolutional]
+
+## Related
+
+- [tensorflow/models](https://github.com/tensorflow/models)
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='tfmodels',
+      version='0.1',
+      description='I do not understand things I can not build.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research'
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'],
+      keywords='tensorflow, artificial intelligence,',
+      url='https://github.com/gongzhitaao/tfmodels',
+      author='Zhitao Gong',
+      author_email='zhitaao.gong@gmail.com',
+      license='MIT',
+      packages=['tfmodels'],
+      install_requires=[
+          'tensorflow',
+          'numpy'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      include_package_data=True,
+      zip_safe=False)
